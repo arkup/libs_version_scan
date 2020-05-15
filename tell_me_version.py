b205ad7a0e282229e8855d86085f1aa3fce410e6
arkup <36083957+arkup@users.noreply.github.com>
2020-05-15 21:11:44 +0200

    small changes

--- tell_me_version.py
+++ tell_me_version.py
@@ -3,11 +3,14 @@ import re
 
 VERSION_SCAN_DIR = r'c:\outdated_libs'
 
-#e.g. "version 3.2.5"
+# e.g. "version 3.2.5"
 pattern_tres_dot = re.compile(b"version\s\d[.]\d[.]\d[.]*")
 
-#e.g. MISC_1.0
+# e.g. MISC_1.0
 pattern_with_underscore = re.compile(b"[a-zA-Z0-9]*_\d[.]\d[.]{0,1}\d*")
+
+# e.g. OpenSSL 1.0.1e
+pattern_ssl = re.compile(b"OpenSSL\s\d[.]\d[.]\d[.]*.")
 
 so_files = []
 
@@ -23,7 +26,7 @@ for r, d, f in os.walk(VERSION_SCAN_DIR):
             continue
         if '.so' in file:
             so_files.append(os.path.join(r, file))
-        else:   #or include all files no just .so
+        else:
             so_files.append(os.path.join(r, file))
 
 #
@@ -42,15 +45,16 @@ for f in so_files:
                 continue
 
             r1 = re.findall(pattern_tres_dot, s)
+            r2 = re.findall(pattern_with_underscore, s)
+            r3 = re.findall(pattern_ssl, s)
             if len(r1):
                 ver_info = f + "  " + r1[0].decode("utf-8")
                 print(ver_info)
-            else:
-                r1 = re.findall(pattern_with_underscore, s)
+            if len(r2):
                 prev_ver_info = ""
-                for i in range(len(r1)):
-                    if len(r1[i]):
-                        lib_filename = r1[i].decode("utf-8")
+                for i in range(len(r2)):
+                    if len(r2[i]):
+                        lib_filename = r2[i].decode("utf-8")
                         if lib_filename.startswith('GLIBC'):
                             continue
                         elif lib_filename.startswith("ARM_"):
@@ -73,10 +77,22 @@ for f in so_files:
                         #    continue
                         else:
                             # don't print duplicates
-                            ver_info = f + "  " + r1[i].decode("utf-8")
+                            ver_info = f + "  " + r2[i].decode("utf-8")
                             if ver_info == prev_ver_info:
                                 continue
                             print(ver_info)
                             prev_ver_info = ver_info
+
+            if len(r3):
+                prev_ver_info = ""
+                for i in range(len(r3)):
+                    if len(r3[i]):
+                        ver_info = f + "  " + r3[i].decode("utf-8")
+                        if ver_info == prev_ver_info:
+                            continue
+                        print(ver_info)
+                        prev_ver_info = ver_info
+
+
     except:
         pass
